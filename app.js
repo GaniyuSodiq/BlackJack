6d6cdb72ca23f572e21a3123c3cdc5aac6fb5c06
Sodiq Ganiyu <gsolaniyi@gmail.com>
2023-03-20 10:22:22 +0100

    change the if console to message

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 100
 let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
+let message 
 
 console.log(sum)
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card?")
+    message = "Do you want to draw a new card?"
 } else if (sum === 21) {
-    console.log("You've got BlackJack!")
+    message = "You've got BlackJack!"
     hasBlackjack = true
 } else {
-    console.log("You're out of the game!")
+    message ="You're out of the game!"
     isAlive = false
 }
 
-console.log(hasBlackjack)
+console.log(message)
