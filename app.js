8797f912060c53127118115a8a61903ce8e32b0f
Sodiq Ganiyu <gsolaniyi@gmail.com>
2023-03-20 10:06:33 +0100

    add isAlive as another game state tester

--- app.js
+++ app.js
@@ -2,19 +2,18 @@ let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackjack = false
+let isAlive = true
+
 console.log(sum)
 
 if (sum <= 20) {
     console.log("Do you want to draw a new card?")
-} 
-
-else if (sum === 21) {
+} else if (sum === 21) {
     console.log("You've got BlackJack!")
     hasBlackjack = true
-} 
-
-else {
+} else {
     console.log("You're out of the game!")
+    isAlive = false
 }
 
 console.log(hasBlackjack)
