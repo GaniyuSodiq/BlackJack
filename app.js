321079485666d628731d059fdb88014a0cd9cca2
Sodiq Ganiyu <gsolaniyi@gmail.com>
2023-03-20 10:02:32 +0100

    testing the state of the game using a boolean variable

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 let firstCard = 10
-let secondCard = 22
-
+let secondCard = 11
 let sum = firstCard + secondCard
-
+let hasBlackjack = false
 console.log(sum)
 
-if (sum < 21) {
+if (sum <= 20) {
     console.log("Do you want to draw a new card?")
-} else if (sum === 21) {
+} 
+
+else if (sum === 21) {
     console.log("You've got BlackJack!")
-} else if (sum > 21) {
+    hasBlackjack = true
+} 
+
+else {
     console.log("You're out of the game!")
 }
+
+console.log(hasBlackjack)
